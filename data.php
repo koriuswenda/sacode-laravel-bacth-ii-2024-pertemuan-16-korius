05de01c7dec4013ece3314eb4a2602b64f154037
korius kussa wenda <koriuswenda@gmail.com>
2024-09-30 18:08:29 +0700

    feat:tambah data crud

---
+++ data.php
@@ -0,0 +1,38 @@
+<?php
+
+
+$mahasiswa = [
+
+    [
+        'id' => 1,
+        'nim' => "2019051074005",
+        'nama' => "Korius Wenda",
+        'alamat' => "Padang Bulan",
+        'jurusan' => "Sistem Informasi"
+    ],
+
+    [
+        'id' => 2,
+        'nim' => "16411046",
+        'nama' => "Samuel Bosawer",
+        'alamat' => "Sentani",
+        'jurusan' => "Teknik Informatika"
+    ],
+
+    [
+        'id' => 3,
+        'nim' => "16411021",
+        'nama' => "Paulus Sesa",
+        'alamat' => "Waena",
+        'jurusan' => "Sistem Informasi"
+    ],
+
+    [
+        'id' => 4,
+        'nim' => "16411121",
+        'nama' => "Billy Rumbiak",
+        'alamat' => "Kotaraja",
+        'jurusan' => "Sistem Informasi"
+    ],
+
+];
